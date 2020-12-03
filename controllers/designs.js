509a8e6c40d8ea438e83c0646f3f0a136e14f36d
Coby Kabili <ckabili8@gmail.com>
2020-12-02 17:35:33 -0700

    style header main and footer

--- controllers/designs.js
+++ controllers/designs.js
@@ -2,8 +2,24 @@ const Design = require('../models/design');
 
 module.exports = {
     index,
+    new: newDesign,
+    show,
+    create,
 };
 
 function index(req, res) {
-    res.render('designs/index', {title: 'Home'})
+    let user = req.user
+    res.render('designs/index', {title: 'Home', user})
 }
+
+function newDesign(req, res) {
+
+};
+
+function show(req, res) {
+
+};
+
+function create(req, res) {
+
+};
--- routes/designs.js
+++ routes/designs.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const designsCtrl = require('../controllers/designs');
+const isLoggedIn = require('../config/auth')
 
 router.get('/', designsCtrl.index);
+router.get('/new', isLoggedIn, designsCtrl.new);
+router.get(':id', designsCtrl.show);
+router.post('/', isLoggedIn, designsCtrl.create);
 
 module.exports = router;
